aebba32247cf1ff69a76cb3ee0280151708f3729
Eric Abell <ESAWrite@gmail.com>
2020-10-18 13:34:04 -0400

    Added images into HTML

--- js/00s-movies.js
+++ js/00s-movies.js
@@ -1,31 +1,31 @@
-// Hold trivia game questions, answers, and stock photos for 00s movies.
+// 1. Hold trivia game questions, answers, and stock photos for 00s movies.
 const aughtQuestions = [
     {
         question: "Which movie earned more at the box office in 2001?\n(a) The Lord of the Rings: The Fellowship of the Ring\n(b) Harry Potter and the Sorcerer's Stone",
         answer: "b",
         value: "Harry Potter and the Sorcerer's Stone earned $974,755,371 and The Lord of the Rings: The Fellowship of the Ring earned $883,726,270",
-        photo: "img/Die-Hard.jpg",
+        photo: "img/HP-Stone.jpg",
         alt: "Harry Potter and the Sorcerer's Stone"
     },
     {
         question: "Which movie earned more at the box office in 2002?\n(a) The Lord of the Rings: The Two Towers\n(b) Harry Potter and the Chamber of Secrets",
         answer: "a",
         value: "The Lord of the Rings: The Two Towers earned $936,689,735 and Harry Potter and the Chamber of Secrets earned $878,979,634",
-        photo: "img/Die-Hard.jpg",
+        photo: "img/LOTR-Two-Towers.jpeg",
         alt: "The Lord of the Rings: The Two Towers"
     },
     {
         question: "Which movie earned more at the box office in 2006?\n(a) The Departed\n(b) Casino Royale",
         answer: "b",
         value: "Casino Royale earned $606,099,584 and The Departed earned $291,465,034",
-        photo: "img/Die-Hard.jpg",
+        photo: "img/Casino-Royale.jpg",
         alt: "Casino Royale"
     },
     {
         question: "Which movie earned more at the box office in 2008?\n(a) Iron Man\n(b) The Dark Knight",
         answer: "b",
         value: "The Dark Knight earned $1,003,045,358 and Iron Man earned $585,366,247",
-        photo: "img/Die-Hard.jpg",
+        photo: "img/The-Dark-Knight.jpg",
         alt: "The Dark Knight"
     },  
 ];
@@ -43,11 +43,12 @@ for(let i = 0; i < aughtQuestions.length; i++) {
     }
 }
 
+// 3. Display number of questions correct to the user
 let html1 = `<h1>You got ${correctAnswers} questions correct!</h1>`;
 
 document.querySelector('main').innerHTML = html1;
 
-//3. Display the correct answers to the user.  
+// 4. Display the correct answers to the user.  
 let html2 = '';
 
 for(let i = 0; i < aughtQuestions.length; i++) {
--- js/90s-movies.js
+++ js/90s-movies.js
@@ -1,31 +1,31 @@
-// Hold trivia game questions, answers, and stock photos for 90s movies.
+// 1. Hold trivia game questions, answers, and stock photos for 90s movies.
 const ninetiesQuestions = [
     {
         question: "Which movie earned more at the box office in 1990?\n(a) Home Alone\n(b) Pretty Woman",
         answer: "a",
         value: "Home Alone earned $476,684,675 and Pretty Woman earned $463,406,268",
-        photo: "img/Die-Hard.jpg",
+        photo: "img/Home-Alone.jpg",
         alt: "Home Alone"
     },
     {
         question: "Which movie earned more at the box office in 1994?\n(a) The Lion King\n(b) Forrest Gump",
         answer: "a",
         value: "The Lion King earned $763,455,561 and Forrest Gump earned $677,387,716",
-        photo: "img/Die-Hard.jpg",
+        photo: "img/The-Lion-king.jpg",
         alt: "The Lion King"
     },
     {
         question: "Which movie earned more at the box office in 1995?\n(a) Jumani\n(b) Toy Story",
         answer: "b",
         value: "Toy Story earned $363,007,140 and Jumanji $262,797,249",
-        photo: "img/Die-Hard.jpg",
+        photo: "img/Toy-Story.jpg",
         alt: "Toy Story"
     },
     {
         question: "Which movie earned more at the box office in 1999?\n(a) The Matrix\n(b) The Mummy",
         answer: "a",
         value: "The Matrix earned $463,517,383 and The Mummy earned $415,933,406",
-        photo: "img/Die-Hard.jpg",
+        photo: "img/The-Matrix.jpg",
         alt: "The Matrix"
     },  
 ];
@@ -43,11 +43,12 @@ for(let i = 0; i < ninetiesQuestions.length; i++) {
     }
 }
 
+// 3. Display number of questions correct to the user
 let html1 = `<h1>You got ${correctAnswers} questions correct!</h1>`;
 
 document.querySelector('main').innerHTML = html1;
 
-//3. Display the correct answers to the user.  
+// 4. Display the correct answers to the user.  
 let html2 = '';
 
 for(let i = 0; i < ninetiesQuestions.length; i++) {
